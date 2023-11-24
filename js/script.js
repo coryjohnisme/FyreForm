8f8f662b1f578ee0c77e0b9d2571e108d80b42bc
coryjohnisme <96593907+coryjohnisme@users.noreply.github.com>
2023-11-24 00:33:46 -0500

    form

--- js/script.js
+++ js/script.js
@@ -1,24 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
 
-        // Get all the FAQ links
-        const faqLinks = document.querySelectorAll('.faq-link');
-      
-        // Add click event listener to each FAQ link
-        faqLinks.forEach(link => {
-          link.addEventListener('click', function (event) {
-            // Prevent the default link behavior (e.g., navigating to a new page)
-            event.preventDefault();
-      
-            // Get the parent .faq element
-            const faq = this.closest('.faq');
-      
-            // Get the .answer element within the same .faq element
-            const answer = faq.querySelector('.answer');
-      
-            // Toggle the display style of the .answer element
-            answer.style.display = (answer.style.display === 'block') ? 'none' : 'block';
-      
-            // Toggle the rotate class on the img element
-            const img = this.querySelector('img');
-            img.classList.toggle('rotate');
-          });
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: 'smooth',
+          timingFunction: 'cubic-bezier(0.42, 0, 0.58, 1)'
         });
+      }
+    });
+  });
+
+  const faqLinks = document.querySelectorAll('.faq-link');
+  faqLinks.forEach(link => {
+    link.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      const faq = this.closest('.faq');
+      const answer = faq.querySelector('.answer');
+      answer.style.display = (answer.style.display === 'block') ? 'none' : 'block';
+
+      const img = this.querySelector('img');
+      img.classList.toggle('rotate');
+    });
+  });
+});
